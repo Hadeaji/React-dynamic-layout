c242fdb1b8dd4b598f5e0c927876dacb00d7b519
Hadi aji <hadeaji@gmail.com>
2023-11-20 23:10:41 +0300

    too messy working demo

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+// import App from './App.tsx'
 import './index.css'
+import AltApp from './AltApp.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <AltApp domElements={[]} containerPadding={[12, 12]} />
   </React.StrictMode>,
 )
