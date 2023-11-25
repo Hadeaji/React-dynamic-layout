b1a9859df01e93effb684fef93156fc202e9ead4
Hadi aji <hadeaji@gmail.com>
2023-11-25 22:52:16 +0300

    phase 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App.tsx'
 import './index.css'
-import AltApp from './AltApp.tsx'
+import ResponsiveLayout from './ResponsiveLayout.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
-    <AltApp domElements={[]} containerPadding={[12, 12]} />
+    <ResponsiveLayout domElements={[]} containerPadding={[12,12]} >
+    <div
+        key={"sad"}
+        style={{ background: "#ccc" }}
+        // data-grid={}
+        // className={l.static ? "static" : ""}
+      >
+        <span className="text">{1}</span>
+      </div>
+    </ResponsiveLayout>
   </React.StrictMode>,
 )
